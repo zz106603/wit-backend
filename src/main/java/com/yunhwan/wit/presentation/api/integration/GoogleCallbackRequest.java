0c078759608dd2d43b2884418d0d652acfe73c9a
YUNHWAN <629jyh7@naver.com>
2026-04-07 13:24:09 +0900

    del: OAuth, Calender 임시 연동 stub 파일 삭제

--- src/main/java/com/yunhwan/wit/presentation/api/integration/GoogleCallbackRequest.java
+++
@@ -1,9 +0,0 @@
-package com.yunhwan.wit.presentation.api.integration;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record GoogleCallbackRequest(
-        @NotBlank(message = "code는 필수입니다.")
-        String code
-) {
-}
